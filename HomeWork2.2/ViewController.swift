52fd86329e71d423536a5760a944dd5a90b4117d
Paul <pasha.makey@gmail.com>
2023-11-09 21:26:50 +0300

    App logic is ready

--- HomeWork2.2/ViewController.swift
+++ HomeWork2.2/ViewController.swift
@@ -7,12 +7,57 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+enum CurrentLight {
+    case red
+    case yellow
+    case green
+}
 
+final class ViewController: UIViewController {
+    
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    
+    @IBOutlet var startButtonView: UIButton!
+    
+    let lightIsOn: CGFloat = 1
+    let lightIsOff: CGFloat = 0.3
+    
+    var currentLight = CurrentLight.green
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
     }
-
-
+    
+    func setupUI() {
+        redView.layer.cornerRadius = redView.frame.width / 2
+        yellowView.layer.cornerRadius = yellowView.frame.width / 2
+        greenView.layer.cornerRadius = greenView.frame.width / 2
+        
+        redView.alpha = lightIsOff
+        yellowView.alpha = lightIsOff
+        greenView.alpha = lightIsOff
+    }
+    
+    @IBAction func startButton() {
+        startButtonView.setTitle("Next", for: .normal)
+        switch currentLight {
+            case .red:
+                redView.alpha = lightIsOff
+                yellowView.alpha = lightIsOn
+                currentLight = .yellow
+            case .yellow:
+                yellowView.alpha = lightIsOff
+                greenView.alpha = lightIsOn
+                currentLight = .green
+            case .green:
+                greenView.alpha = lightIsOff
+                redView.alpha = lightIsOn
+                currentLight = .red
+        }
+    }
 }
+
 
