77bc7f6c964d4a74317d629d3e26bc20f3b4a25c
Paul <pasha.makey@gmail.com>
2023-11-14 21:00:24 +0300

    Made a new file for extension

---
+++ Extension + ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  Extension + ViewController.swift
+//  HomeWork2.2
+//
+//  Created by Paul Makey on 14.11.23.
+//
+
+import UIKit
+
+// MARK: - CurrentLight
+extension ViewController {
+    enum CurrentLight {
+        case red
+        case yellow
+        case green
+    }
+}
+
+// MARK: - Extensions guide
+/* Extensions
+    1. Возможность расширить тип данных
+    2. Группировка методов по их назначению
+    3. Возможность работать в разных файлах, что помогает при командной работе
+ */
